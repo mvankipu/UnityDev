c76290ef14ad682ce99eda4dd5904876bab918e4
Mithra Vankipuram <vankipuram.mithra@gmail.com>
2020-04-11 12:03:47 -0700

    Made minor modifications to HelloWorldPlusPlus camera background color is set using script

---
+++ 2_HelloWorldPlusPlus/Assets/Scripts/HelloWorldPlusPlus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorldPlusPlus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera tempCam = GetComponent<Camera>();
+        
+        tempCam.backgroundColor = new Color(1,1,1,1);
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Debug.Log("Application will quit.");
+            Application.Quit();
+        }
+    }
+}
---
+++ 2_HelloWorldPlusPlus/Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //UnityEngine.Debug.Log("Mouse Down");
+
+            //onMouseDown Event detected
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                //onMouseDown Event detected on object
+                //UnityEngine.Debug.Log("Mouse Down on Object " + hit.collider.gameObject.ToString());
+
+                //Change Material of Game Object
+                hit.collider.gameObject.GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            }
+            else
+            {
+                Camera tempCam = GetComponent<Camera>();
+                tempCam.backgroundColor = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            }
+        }
+    }
+}
