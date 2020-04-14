6160a96bb1dc3b410e23815fee7925f5b3887a10
Mithra Vankipuram <vankipuram.mithra@gmail.com>
2020-04-14 00:34:49 -0700

    WIP: Four blocks game

---
+++ 3_FourBlocksGame/Assets/Scripts/FourBlocks.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class FourBlocks : MonoBehaviour
+{
+    enum GameState
+    {
+        start, //Display welcome screen with instructions
+        //prepTimer, //After user clocks start button show a 3,2,1... timer 
+        ready, //Show pattern
+        getUserInput, //listen for user input in this state
+        winScreen, //Check if user got it right and show appropriate display  
+        end, //Thanks for playing
+    }
+
+    Pattern testPattern;
+    GameState currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = GameState.start;
+
+        //Tests for Test Pattern constructor
+        //testPattern = new Pattern();
+        //testPattern = new Pattern(7,1);
+        //testPattern = new Pattern(5,10,14);
+
+        testPattern = new Pattern(7,1,4);
+
+        //TODO Move this to the event handler for the start button when implemented
+        testPattern.startPattern();
+        testPattern.print();
+        
+        currentState = GameState.ready;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("escape"))
+        {
+            UnityEngine.Debug.Log("Application will quit.");
+            Application.Quit();
+        }
+
+        if(currentState == GameState.ready)
+        {
+            //Display Pattern
+            int currentPatternItem = testPattern.getActivePatternItem(Time.deltaTime);
+
+            //TODO Set all cube materials to default
+            GameObject.Find("Cube1").GetComponent<Renderer>().material = Resources.Load<Material>("Red1");
+            GameObject.Find("Cube2").GetComponent<Renderer>().material = Resources.Load<Material>("Blue1");
+            GameObject.Find("Cube3").GetComponent<Renderer>().material = Resources.Load<Material>("Yellow1");
+            GameObject.Find("Cube4").GetComponent<Renderer>().material = Resources.Load<Material>("Green1");
+
+            if (currentPatternItem == -1)
+            {
+                UnityEngine.Debug.Log("[FourBlocks] Pattern ended. Ready for user input.");
+                currentState = GameState.getUserInput;
+            }
+            else if (currentPatternItem == 0)
+            {
+                //Do Nothing
+            }
+            else
+            {
+                //UnityEngine.Debug.Log(currentPatternItem.ToString());
+                int blockId = testPattern.getPatternListItem(currentPatternItem);
+                
+                if (blockId == 1)
+                {
+                    GameObject.Find("Cube1").GetComponent<Renderer>().material = Resources.Load<Material>("Red2");
+                }
+                else if (blockId == 2)
+                {
+                    GameObject.Find("Cube2").GetComponent<Renderer>().material = Resources.Load<Material>("Blue2");
+                }
+                else if (blockId == 3)
+                {
+                    GameObject.Find("Cube3").GetComponent<Renderer>().material = Resources.Load<Material>("Yellow2");
+                }
+                else if (blockId == 4)
+                {
+                    GameObject.Find("Cube4").GetComponent<Renderer>().material = Resources.Load<Material>("Green2");
+                }
+                else
+                {
+                    GameObject.Find("Cube1").GetComponent<Renderer>().material = Resources.Load<Material>("Red1");
+                    GameObject.Find("Cube2").GetComponent<Renderer>().material = Resources.Load<Material>("Blue1");
+                    GameObject.Find("Cube3").GetComponent<Renderer>().material = Resources.Load<Material>("Yellow1");
+                    GameObject.Find("Cube4").GetComponent<Renderer>().material = Resources.Load<Material>("Green1");
+                }
+            }
+        }
+
+        //if state is ready
+
+        //create a random pattern of length 3
+
+        //show pattern
+        //highlight 1, timer, 5second - increament display counter, highlight cubes[i]...
+         
+
+    }
+}
---
+++ 3_FourBlocksGame/Assets/Scripts/Pattern.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class Pattern
+{
+	int[] patternList;
+
+	enum PatternState
+    {
+		active,
+		inactive,
+    }
+
+	PatternState pState;
+	float startTime;
+	float bufferTime;
+	float inactiveTime;
+	float onDeltaTime;
+	float offDeltaTime;
+	int activePatternItem;
+
+	public Pattern(int patternLength = 3, int rangeMin = 1, int rangeMax = 4)
+	{
+		activePatternItem = -1;
+
+		patternList = new int[patternLength];
+		
+		for(int i=0;i<patternLength;i++)
+        {
+			patternList[i] = UnityEngine.Random.Range(rangeMin, rangeMax + 1);
+		}
+	}
+	
+	public void startPattern(float onTime = 2.0f, float offTime = 1.0f)
+    {
+		activePatternItem = 1;
+		onDeltaTime = onTime;
+		offDeltaTime = offTime;
+		startTime = onDeltaTime;
+		bufferTime = offDeltaTime;
+		pState = PatternState.active;
+		//UnityEngine.Debug.Log(patternList[activePatternItem - 1].ToString());
+	}
+
+	
+	public int getActivePatternItem(float deltaTime)
+    {
+		if(pState == PatternState.active)
+        {
+			startTime -= deltaTime;
+			if (startTime <= 0.0)
+			{
+				activePatternItem++;
+
+				//UnityEngine.Debug.Log(patternList[activePatternItem - 1].ToString());
+				bufferTime = offDeltaTime;
+				pState = PatternState.inactive;
+				return 0;
+			}
+            else
+            {
+				return activePatternItem;
+            }
+		}
+		else
+        {
+			bufferTime -= deltaTime;
+			if (bufferTime <= 0.0)
+			{
+				if (activePatternItem > patternList.Length) //TODO WHY DOES THIS WORK?????
+					return -1;
+				else
+                {
+					startTime = onDeltaTime;
+					pState = PatternState.active;
+					return activePatternItem;
+				}
+			}
+			else
+				return 0;
+		}
+		//return activePatternItem;
+    }
+
+	public int getLength()
+    {
+		return patternList.Length;
+    }
+
+	public int getPatternListItem(int index)
+    {
+		//TODO Error Checking needed
+		//UnityEngine.Debug.Log(index.ToString());
+		return patternList[index-1];
+    }
+
+	public void print()
+    {
+		string temp = "Pattern = ";
+		for (int i = 0; i < patternList.Length; i++)
+		{
+			temp += patternList[i].ToString() + " ";
+		}
+		//temp += ": Parameters passed = ( " + patternList.Length.ToString() + " , " + patternList.Min().ToString() + " , " + patternList.Max().ToString() + ")"; 
+		UnityEngine.Debug.Log(temp);
+	}
+}
